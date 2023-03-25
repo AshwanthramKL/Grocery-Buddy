2dd17572c9e2146ccf8b9aed430e23f27f19fa0f
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-03-25 20:48:23 +0530

    added format for addItem

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ****** SELECT ITEMS **********
 const alert = document.querySelector(".alert");
-const form = document.querySelector('.grocery-form');
+const form = document.querySelector(".grocery-form");
 const grocery = document.querySelector('#grocery');
 const submitBtn = document.querySelector('.submit-btn');
 const container = document.querySelector('.grocery-container');
@@ -13,9 +13,24 @@ let editFlag = false;
 let editID = '';
 
 // ****** EVENT LISTENERS **********
-
+form.addEventListener("submit", addItem);
 // ****** FUNCTIONS **********
+function addItem(e){
+    e.preventDefault();
+    const value = grocery.value;
 
+    const id = new Date().getTime().toString();
+
+    if(value && editFlag){
+        console.log('edit item');
+    }
+    else if(value && !editFlag){
+        console.log('add item');            
+    }
+    else{
+        console.log('empty value');
+    }
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
